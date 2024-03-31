f0df4f08ba13bb873c48c353c23f8e3b8a662d21
DongYeop <s01054830455@gmail.com>
2024-03-31 23:51:08 +0900

    setting

---
+++ src/main/java/com/tasty/reviews/tastyreviews/TastyReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.tasty.reviews.tastyreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyReviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasty/reviews/tastyreviews/TastyReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasty.reviews.tastyreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
