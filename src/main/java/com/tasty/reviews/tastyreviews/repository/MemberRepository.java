f521a04658f3a4beb1a0198f3af06e3407f39375
DongYeop <dlehdduq0455@naver.com>
2024-04-09 14:20:01 +0900

    이메일 중복 메서드 추가

--- src/main/java/com/tasty/reviews/tastyreviews/repository/MemberRepository.java
+++ src/main/java/com/tasty/reviews/tastyreviews/repository/MemberRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
     Optional<Member> findByEmail(String email); //이메일과 일치하는 회원을 찾아줌
+    boolean existsByEmail(String email); //이메일 존재여부 확인
 }
