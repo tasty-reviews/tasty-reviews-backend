d0f4c07c2b3d2b5e2e31a9dcb30945de9e464f9a
DongYeop <dlehdduq0455@naver.com>
2024-04-09 21:11:50 +0900

    시큐리티 설정 변경

--- src/main/java/com/tasty/reviews/tastyreviews/config/SecurityConfig.java
+++ src/main/java/com/tasty/reviews/tastyreviews/config/SecurityConfig.java
@@ -1,15 +1,31 @@
 package com.tasty.reviews.tastyreviews.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     //패스워드 암호 메서드
     @Bean
@@ -20,16 +36,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.
-                authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/join").permitAll()
+        http
+                .formLogin(AbstractHttpConfigurer::disable) //form 로그인 방식 disable
+                .httpBasic(AbstractHttpConfigurer::disable) //http 인증 방식 disable
+                .csrf(AbstractHttpConfigurer::disable) //csrk disable
+
+                .authorizeHttpRequests(
+                        (auth) -> auth
+                                .requestMatchers("/login", "/", "/join")
+                                .permitAll()
+                                .anyRequest().authenticated()
                 );
 
-        //csrf 토큰 disable
         http
-                .csrf((auth) -> auth.disable());
-
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
+
 }
