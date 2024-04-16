b9a25e8bd4b71d68001e269eedbb4f489c6eec75
DongYeop <s01054830455@gmail.com>
2024-04-16 13:53:29 +0900

    feat: 로그인 성공시 refresh 토큰 발급 메서드 추가

--- src/main/java/com/tasty/reviews/tastyreviews/jwt/LoginFilter.java
+++ src/main/java/com/tasty/reviews/tastyreviews/jwt/LoginFilter.java
@@ -1,10 +1,11 @@
 package com.tasty.reviews.tastyreviews.jwt;
 
-import com.tasty.reviews.tastyreviews.dto.CustomUserDetails;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -40,21 +41,22 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
 
-        // 인증된 사용자 정보 추출
-        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-        String username = customUserDetails.getUsername();
+        //유저 정보
+        String username = authentication.getName();
 
-        // 권한 정보 추출
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
         GrantedAuthority auth = iterator.next();
         String role = auth.getAuthority();
 
-        // JWT 생성
-        String token = jwtUtil.createJwt(username, role, 60 * 60 * 10L);
+        //토큰 생성
+        String access = jwtUtil.createJwt("access", username, role, 600000L);
+        String refresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
-        // 생성된 JWT를 응답 헤더에 추가
-        response.addHeader("Authorization", "Bearer " + token);
+        //응답 설정
+        response.setHeader("access", access); //헤더에 access 토큰
+        response.addCookie(createCookie("refresh", refresh)); //쿠키에 refresh 토큰
+        response.setStatus(HttpStatus.OK.value()); //상태코드 설정
     }
 
     // 로그인 실패 시 실행되는 메서드
@@ -62,5 +64,16 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
         //로그인 실패시 401 응답 코드 반환
         response.setStatus(401);
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24 * 60 * 60);
+        //cookie.setSecure(true);
+        //cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
