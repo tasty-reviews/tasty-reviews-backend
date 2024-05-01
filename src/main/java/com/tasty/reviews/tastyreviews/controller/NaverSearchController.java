c0b5644d5d968420f045c73d8ef8194bcc9c6cd8
DongYeop <s01054830455@gmail.com>
2024-05-01 23:25:38 +0900

    .

--- src/main/java/com/tasty/reviews/tastyreviews/controller/NaverSearchController.java
+++ src/main/java/com/tasty/reviews/tastyreviews/controller/NaverSearchController.java
@@ -2,7 +2,6 @@ package com.tasty.reviews.tastyreviews.controller;
 
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +11,6 @@ import java.net.URI;
 import java.nio.charset.Charset;
 
 @RestController
-@RequestMapping
 public class NaverSearchController {
 
     @GetMapping(value = "/search", produces = "application/json; charset=UTF-8")
@@ -33,7 +31,7 @@ public class NaverSearchController {
                 .build()
                 .toUri();
 
-
+        //요청헤더에 API id, 시크릿키 추가
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-Naver-Client-Id", clientId);
         headers.set("X-Naver-Client-Secret", clientSecret);
