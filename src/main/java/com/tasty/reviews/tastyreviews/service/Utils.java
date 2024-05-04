52600608f33eba2b1005f96094fbb91a1526cd5a
wodudam <wodudam@naver.com>
2024-05-05 02:44:15 +0900

    remove: Utils삭제

--- src/main/java/com/tasty/reviews/tastyreviews/service/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.tasty.reviews.tastyreviews.service;
-
-public class Utils {
-    // HTML 태그를 제거하는 메소드
-    public static String removeHtmlTags(String input) {
-        return input.replaceAll("<[^>]*>", "");
-    }
-}
-
