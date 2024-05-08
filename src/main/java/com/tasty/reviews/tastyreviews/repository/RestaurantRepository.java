fa2504752e860982bdda0f2bae5d0b8b7d307bf1
wodudam <wodudam@naver.com>
2024-05-08 22:15:19 +0900

    fix: Restaurant 엔티티, 리포지토리 수정
    fix: Restaurant 엔티티, 리포지토리 수정
    
    fix: Restaurant 엔티티, 리포지토리 수정

--- src/main/java/com/tasty/reviews/tastyreviews/repository/RestaurantRepository.java
+++ src/main/java/com/tasty/reviews/tastyreviews/repository/RestaurantRepository.java
@@ -1,13 +1,15 @@
 package com.tasty.reviews.tastyreviews.repository;
 
 import com.tasty.reviews.tastyreviews.domain.Restaurant;
+import com.tasty.reviews.tastyreviews.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
-    Optional<Restaurant> findByNameAndAddress(String name, String address);
+    Optional<Restaurant> findByPlaceNameAndRoadAddressName(String placeName, String roadAddressName);
 }
