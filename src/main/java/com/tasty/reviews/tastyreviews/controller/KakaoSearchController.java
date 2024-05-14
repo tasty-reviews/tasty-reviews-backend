1d56db7c75390ab2248c8fef05b0c5bb534983bd
wodudam <wodudam@naver.com>
2024-05-14 14:12:11 +0900

    fix: 수정

--- src/main/java/com/tasty/reviews/tastyreviews/controller/KakaoSearchController.java
+++ src/main/java/com/tasty/reviews/tastyreviews/controller/KakaoSearchController.java
@@ -21,36 +21,6 @@ public class KakaoSearchController {
 
     private final RestaurantService restaurantService;
 
-    /*@GetMapping("/search")
-    public ResponseEntity<String> getKakaoApiFromAddress(@RequestParam("query") String keyword) {
-
-        try {
-            URI uri = UriComponentsBuilder.fromUriString(KAKAO_API_URL)
-                    .queryParam("query", keyword)
-                    .encode(StandardCharsets.UTF_8)
-                    .build()
-                    .toUri();
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Authorization", "KakaoAK " + API_KEY); // 요청 헤더에 API 키 추가
-
-            HttpEntity<?> entity = new HttpEntity<>(headers);
-
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
-
-            // 개선: 응답이 성공적으로 수신되었는지 확인
-            if (responseEntity.getStatusCode() == HttpStatus.OK) {
-                return ResponseEntity.ok(responseEntity.getBody());
-            } else {
-                return ResponseEntity.status(responseEntity.getStatusCode()).body("API 호출 실패");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류");
-        }
-    }*/
-
     @GetMapping("/search")
     public ResponseEntity<String> getKakaoApiFromAddress(@RequestParam("query") String keyword) {
         try {
--- src/main/java/com/tasty/reviews/tastyreviews/service/RestaurantService.java
+++ src/main/java/com/tasty/reviews/tastyreviews/service/RestaurantService.java
@@ -20,10 +20,6 @@ public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
 
     public RestaurantDTO addRestaurant(RestaurantDTO restaurantDTO) {
-/*        // 사용자 역할 확인
-        if (!isAdmin(username)) {
-            throw new IllegalStateException("Only administrators can add restaurants.");
-        }*/
         // DTO를 엔티티로 변환
         Restaurant restaurant = restaurantDTO.toEntity();
 
@@ -60,11 +56,4 @@ public class RestaurantService {
         }
     }
 
-
-/*    private boolean isAdmin(String username) {
-        // 여기에 사용자 이름(username)을 기반으로 사용자 역할을 확인하는 로직을 추가하세요.
-        // 예를 들어, 사용자 이름으로 데이터베이스에서 해당 사용자를 조회하여 역할을 확인할 수 있습니다.
-        // 이 예제에서는 단순히 "admin"이라는 이름을 가진 사용자만이 관리자로 간주합니다.
-        return "admin".equals(username);
-    }*/
 }
