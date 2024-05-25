131d9a947c5be660acbba2eb36929e1fd7072229
wodudam <wodudam@naver.com>
2024-05-26 01:29:18 +0900

    fix: RankingController restcontroller로 수정
    내지도에 저장된 음식점을 조회할 때 내지도의 상세정보가 출력되지 않는 문제 해결

--- src/main/java/com/tasty/reviews/tastyreviews/restaruant/controller/RankingController.java
+++ src/main/java/com/tasty/reviews/tastyreviews/restaruant/controller/RankingController.java
@@ -5,15 +5,15 @@ import com.tasty.reviews.tastyreviews.restaruant.domain.Restaurant;
 import com.tasty.reviews.tastyreviews.restaruant.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/rankings")
 public class RankingController {
