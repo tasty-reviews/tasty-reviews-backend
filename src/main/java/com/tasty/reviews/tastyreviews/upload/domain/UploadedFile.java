a2e9837e07de5cefc0fbe55936b53f19d587108b
wodudam <wodudam@naver.com>
2024-05-28 14:01:02 +0900

    feat: review와 연관관계 설정

--- src/main/java/com/tasty/reviews/tastyreviews/upload/domain/UploadedFile.java
+++ src/main/java/com/tasty/reviews/tastyreviews/upload/domain/UploadedFile.java
@@ -1,10 +1,9 @@
 package com.tasty.reviews.tastyreviews.upload.domain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.tasty.reviews.tastyreviews.review.domain.Review;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +17,11 @@ public class UploadedFile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id", nullable = false)
+    @JsonIgnore
+    private Review review;
 
     private String originalFileName;
     private String storedFileName;
